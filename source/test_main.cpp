2a4b78ce621e2b55bffbbdc22454481e59cded54
HuiEng <hui_eng1995@hotmail.com>
2023-07-18 15:31:59 +1000

    do tree test to get file list

--- source/test_main.cpp
+++ source/test_main.cpp
@@ -12,19 +12,15 @@ int test_main(int argc, char *argv[])
     std::ios::sync_with_stdio(false); // No sync with stdio -> faster
 
     string inputFile = args.input_arg;
-    if (args.folder_arg)
+    string delimiter = "/*";
+    string folder = inputFile.substr(0, inputFile.find(delimiter));
+    string ext = inputFile.substr(inputFile.find(delimiter) + delimiter.size(), inputFile.size() - 1);
+    fprintf(stderr, "Reading folder %s\n", folder.c_str());
+    for (const auto &entry : fs::directory_iterator(folder))
     {
-
-        string delimiter = "/*";
-        string folder = inputFile.substr(0, inputFile.find(delimiter));
-        string ext = inputFile.substr(inputFile.find(delimiter) + delimiter.size(), inputFile.size() - 1);
-        fprintf(stderr, "Reading folder %s\n", folder.c_str());
-        for (const auto &entry : fs::directory_iterator(folder))
+        if (entry.path().extension() == ext)
         {
-            if (entry.path().extension() == ext)
-            {
-                cout << entry.path() << endl;
-            }
+            cout << entry.path().stem() << endl;
         }
     }
 
