efb5a52ce7364281760334db9ac89763971397a7
Hui Eng Law <hui_eng1995@hotmail.com>
2025-02-25 18:33:17 +1000

    pbuild -r

--- source/build_partition_main.cpp
+++ source/build_partition_main.cpp
@@ -510,6 +510,7 @@ int build_partition_main(int argc, char *argv[])
             fprintf(stderr, "Please provide output file path\n");
             return 0;
         }
+
         outfile = outfile + buffer;
         bloom_filter bf(parameters);
         outfile = outfile + ".bin";
